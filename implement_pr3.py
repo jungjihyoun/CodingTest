b94849084c9afd7b36652c2e3f5cc58db2f31852
jungjihyoun <jihyoun0602@gmail.com>
2021-01-18 09:38:25 +0900

    구현_게임개발 문제

--- implement_pr3.py
+++ implement_pr3.py
@@ -17,25 +17,46 @@ def left():
     if direction == -1:
         direction = 3
 
+#북 = 0 , 동 = 1 , 남 = 2, 서 = 3 방향 정의
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
 
-# check = []
-# fin = True
-# while True:
-#     # 북쪽으로 이동하는 경우 . 위에가 바다인지 아닌지
-#     if (d == 0):
-#         if (load[sx][sy - 1] == 1):
-#             d = 3
-#             continue
-#         sy = sy + dy[2]
-#         continue
-#     elif (d == 3):
-#         if (load[sx - 1][sy] == 1):
-#             d = 2
-#             continue
-#         sx = sx + dx[2]
-#         continue
-#
-#
-#
-#
+
+count = 1
+turn_time = 0
+while True:
+    left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    #회전한 이후 정면에 가보지 않은 칸이 존재하는 경우 이동
+    if (array[nx][ny] == 0 and check[nx][ny] == False ):
+        x = nx
+        y = ny
+        check[nx][ny] = True
+        count += 1
+        turn_time +=1
+        continue
+    else:
+        turn_time += 1
+
+    #네 방향 모두 갈 수 없는 경우
+    if turn_time == 4:
+        nx = x-dx[direction]
+        ny = y - dy[direction]
+        #뒤로 갈 수 있다면 뒤로 이동
+        if array[nx][ny] == 0:
+            x = nx
+            y = ny
+            count += 1
+        #뒤에가 바다로 막혀 있는 경우
+        else:
+            break
+
+        turn_time = 0
+
+print(count)
+
+
+
+
 
